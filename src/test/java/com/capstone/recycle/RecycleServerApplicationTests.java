d8aeb9069601df3b438170f45b653aeea98ce457
BuildTools <unconfigured@null.spigotmc.org>
2026-03-14 16:29:06 +0900

    Initial Spring Boot project setup

---
+++ src/test/java/com/capstone/recycle/RecycleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.recycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
